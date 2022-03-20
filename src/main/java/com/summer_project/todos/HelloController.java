692f60e19ad324867a478004fa9df9889615e397
ullmanf <gusullmfr@student.gu.se>
2022-03-20 20:06:13 +0100

    add frontend

--- src/main/java/com/summer_project/todos/HelloController.java
+++ src/main/java/com/summer_project/todos/HelloController.java
@@ -1,20 +1,23 @@
 package com.summer_project.todos;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//@CrossOrigin(origins = "http://domain2.com", maxAge = 3600)
+//https://spring.io/blog/2015/06/08/cors-support-in-spring-framework
 @RestController
 public class HelloController {
 
+    @CrossOrigin
     @GetMapping("/")
-    public String index() {
-        return "Hey its working";
+    public int index() {
+        return 22;
     }
 
-
+    @CrossOrigin
     @GetMapping("/hello")
-    public String sayHi(){
-        return "hellohello";
+    public int sayHi(){
+        return 33;
     }
 
 
