e5736dacf78c436c29b75b9143a18eac4961e8e5
ullmanf <gusullmfr@student.gu.se>
2022-03-20 10:50:03 +0100

    initial

---
+++ src/main/java/com/summer_project/todos/HelloController.java
@@ -0,0 +1,21 @@
+package com.summer_project.todos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hey its working";
+    }
+
+
+    @GetMapping("/hello")
+    public String sayHi(){
+        return "hellohello";
+    }
+
+
+}
