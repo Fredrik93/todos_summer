76ebc2e8efadd3d8bb6dce1795628243d503152d
ullmanf <gusullmfr@student.gu.se>
2022-03-27 11:45:38 +0200

    add object endpoint

--- src/main/java/com/summer_project/todos/TodosController.java
+++ src/main/java/com/summer_project/todos/TodosController.java
@@ -17,26 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TodosController {
     HashMap<Integer, String> todos = new HashMap<Integer, String>();
-    ArrayList <Todos> listOfTodos = new ArrayList<Todos>();
+    String [] otherList = new String [5];
+    int id= 0;
     Todos t1 = new Todos("");
     public void enterFakeTodos(){
+        otherList[0] = "some other chore";
+        otherList[1] = "and one more";
+
         t1.todos = "clean other stuff";
-        todos.put(1, t1.todos);
+        todos.put(10, t1.todos);
         todos.put(22, "gym");
+        todos.put(20,"Groceries");
     }
     @CrossOrigin
     @GetMapping("/todos")
-    public HashMap index() {
+    public HashMap getAllTodos() {
         enterFakeTodos();
         return todos;
     }
+    
 
     @CrossOrigin
     @PostMapping("/todos")
     public void addTodo(){
-        todos.put(24, "go to school");
+        todos.put(id, "go to school");
+        id++;
+        System.out.println(" id is " + id);
     }
 
+    //and well, this too is unfinished :) 
     @DeleteMapping("/todos")
     public void removeTodo(){
     
